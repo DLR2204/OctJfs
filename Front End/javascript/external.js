72cda28a1b2c016f77bf6d06120c04fbe8778c78
DLR2204 <n130488@rguktn.ac.in>
2024-12-19 19:59:46 +0530

    Add files via upload

---
+++ Front End/javascript/external.js
@@ -0,0 +1,9 @@
+function check(){
+
+window.alert("Hello iam from window alert ");
+
+document.write("Hello iam from document write function");
+
+console.log("Hello im from console log function");
+
+}
